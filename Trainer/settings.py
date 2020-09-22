38820c6f9dc9437b2b2d33f07a56af2b16e79e2e
om_coolkarni <53579014+omcoolkarni22@users.noreply.github.com>
2020-09-22 12:50:47 +0530

    Updated Database Settings

--- Trainer/settings.py
+++ Trainer/settings.py
@@ -74,17 +74,10 @@ WSGI_APPLICATION = 'Trainer.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gym',
-        'USER': 'root',
-        'PASSWORD': '2703',
-        'HOST': 'localhost',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
